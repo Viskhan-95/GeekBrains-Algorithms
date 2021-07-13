c5ba4b9cf521aaa7f8f97d3abac10d1caf180f2c
Viskhan <rustam1987@mail.ru>
2021-07-13 18:53:33 +0300

    sent DZ4

---
+++ DZ4/DZ4.2/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DZ4._2
+{
+    class Program
+    {
+        /*
+         * Не успел реализовать метод удаления и попробовать на роботоспособноть, доработаю.
+         * Единственное не понял, зачем в задании были даны методы TreeHelper и NodeInfo
+         * Нужно было работать с возврашаемым массивом из TreeHelper?
+         * Нужно ли переопределить метод GetHashCode()?
+         */
+        public TreeNode rootNode;
+        public class TreeNode
+        {
+            public int Value { get; set; }
+            public TreeNode LeftChild { get; set; }
+            public TreeNode RightChild { get; set; }
+            public TreeNode Parent { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                var node = obj as TreeNode;
+
+                if (node == null)
+                    return false;
+
+                return node.Value == Value;
+            }
+        }
+
+
+        public interface ITree
+        {
+            TreeNode GetRoot();
+            void AddItem(int value); // добавить узел
+            void RemoveItem(int value); // удалить узел по значению
+            TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+            void PrintTree(); //вывести дерево в консоль
+        }
+
+
+        public static class TreeHelper
+        {
+            public static NodeInfo[] GetTreeInLine(ITree tree)
+            {
+                var bufer = new Queue<NodeInfo>();
+                var returnArray = new List<NodeInfo>();
+                var root = new NodeInfo() { Node = tree.GetRoot() };
+                bufer.Enqueue(root);
+
+                while (bufer.Count != 0)
+                {
+                    var element = bufer.Dequeue();
+                    returnArray.Add(element);
+
+                    var depth = element.Depth + 1;
+
+                    if (element.Node.LeftChild != null)
+                    {
+                        var left = new NodeInfo()
+                        {
+                            Node = element.Node.LeftChild,
+                            Depth = depth,
+                        };
+                        bufer.Enqueue(left);
+                    }
+                    if (element.Node.RightChild != null)
+                    {
+                        var right = new NodeInfo()
+                        {
+                            Node = element.Node.RightChild,
+                            Depth = depth,
+                        };
+                        bufer.Enqueue(right);
+                    }
+                }
+
+                return returnArray.ToArray();
+            }
+        }
+
+
+        public class NodeInfo
+        {
+            public int Depth { get; set; }
+            public TreeNode Node { get; set; }
+        }
+
+
+        TreeNode GetRoot()
+        {
+            return rootNode;
+        }
+
+
+        void AddItem(int value)
+        {
+            var newTreeNode = new TreeNode { Value = value };
+
+            if (rootNode == null)
+            {
+                rootNode = newTreeNode;
+                rootNode.Parent = null;
+                rootNode.LeftChild = null;
+                rootNode.RightChild = null;
+            }
+            else
+            {
+                TreeNode current = rootNode;
+                TreeNode parent;
+
+                while(true)
+                {
+                    parent = current;
+                    if(value < current.Value)
+                    {
+                        current = current.LeftChild;
+
+                        if(current == null)
+                        {
+                            parent.LeftChild = newTreeNode;
+                            break;
+                        }
+                        else
+                        {
+                            current = current.RightChild;
+                            if(current == null)
+                            {
+                                parent.RightChild = newTreeNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        TreeNode GetNodeByValue(int value)
+        {
+            if (rootNode.Value == value)
+                return rootNode;
+            
+            TreeNode current = rootNode;
+            while(true)
+            {
+                if(current.Value > value)
+                {
+                    current = current.LeftChild;
+                    if (current.Value == value)
+                        return current;
+                }
+                else
+                {
+                    current = current.RightChild;
+                    if (current.Value == value)
+                        return current;
+                }
+            }
+        }
+
+
+        void PrintTree(TreeNode treeNode, string str = "")
+        {
+            if(treeNode != null)
+            {
+                Console.WriteLine("{1}{0}", treeNode.Value, str);
+                PrintTree(treeNode.LeftChild, $"{str}{new string(' ', 2)}");
+                PrintTree(treeNode.RightChild, $"{str}{new string(' ', 2)}");
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DZ4/DZ4/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DZ4
+{
+    class Program
+    {
+        static class TestMethods
+        {
+            public static double GetTime(Action act)
+            {
+                /*
+                 * Сначала решил рассчитать среднеарефметическую из 10 запросов на поиск, как в задании к прошлому уроку
+                 * но почему-то в таком случае время на поиск в массиве тратится меньше, чем в hashset
+                 * а если сделать однократный поиск, то hash намного быстрее делает поиск
+                 */
+                Stopwatch sw;
+                sw = new Stopwatch();
+                sw.Start();
+                act();
+                sw.Stop();
+                return sw.ElapsedTicks;
+            }
+        }
+
+
+        static string[] FillingArray(string[] array)
+        {
+            var arrayRandomString = new string[20_000];
+
+            for (int i = 0; i < array.Length; i++)  arrayRandomString[i] = array[i];
+            return arrayRandomString;
+        }
+
+
+        static HashSet<string> FillingНashSet(string[] array)
+        {
+            var hash = new HashSet<string>();
+
+            for (int i = 0; i < array.Length; i++)  hash.Add(array[i]);
+            return hash;
+        }
+
+
+        static string SearchStringInArray(string[] arrayString, string find)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            foreach (var s in arrayString)
+            {
+                if (s == find)
+                {//Можно было создать метод, чтоб не дублировать выводимую на консоль инфо
+                    sw.Stop();
+                    Console.WriteLine($"       Method (поиск)        | Time");
+                    Console.WriteLine($"-----------------------------|-----");
+                    Console.WriteLine($"SearchStringInArray          | {sw.ElapsedTicks}");
+                    
+                    return s;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"       Method (поиск)        | Time");
+            Console.WriteLine($"-----------------------------|-----");
+            Console.WriteLine($"SearchStringInArray          | {sw.ElapsedTicks}");
+            
+            return $"{find} нет в массиве";
+        }
+
+
+        static string SearchStringInHashSet(HashSet<string> hash, string find)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            if (hash.Contains(find))
+            {
+                sw.Stop();
+                Console.WriteLine($"SearchStringInHashSet        | {sw.ElapsedTicks}");
+                return find;
+
+            }     
+            else
+            {
+                sw.Stop();
+                Console.WriteLine($"SearchStringInHashSet        | {sw.ElapsedTicks}");
+                return $"{find} нет в hash";
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Заполнил массив строк здесь, чтоб в SearchArray и SearchHashSet значения были одинаковыми
+            Random rnd = new Random();
+            var randomString = new string[20_000];
+
+            for (int i = 1; i < randomString.Length; i++)
+                randomString[i] = rnd.Next(1, 30_000).ToString();
+
+            double timeArray = 0;
+            double timeHash = 0;
+            
+            timeArray = TestMethods.GetTime(() => SearchStringInArray(FillingArray(randomString), "25890"));
+            timeHash = TestMethods.GetTime(() => SearchStringInHashSet(FillingНashSet(randomString), "25890"));
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Method (заполнение и поиск)  | Time");
+            Console.WriteLine($"-----------------------------|-----");
+            
+            Console.WriteLine($"SearchStringInArray          | {timeArray}");
+            Console.WriteLine($"SearchStringInHashSet        | {timeHash}");
+        }
+    }
+}
