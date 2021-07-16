33e5dcb237556880760214cecedf8334c60138ed
Viskhan <rustam1987@mail.ru>
2021-07-16 18:21:41 +0300

    sent an empty project DZ5

---
+++ DZ5/DZ5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DZ5
+{
+    class Program
+    {
+        //На выходных сделаю и дополнюпере
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
